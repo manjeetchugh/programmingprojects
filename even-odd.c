a3840749943bd796c05352f907af50434b3bff69
Manjeet Chugh <work.manjeetchugh@gmail.com>
2026-09-12 18:32:00 +0530

    Create even-odd.c

---
+++ even-odd.c
@@ -0,0 +1,20 @@
+    #include <stdio.h>
+    void main()
+    {
+        int x;
+    printf("Enter a Number\n");
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+        printf("The given number is even");
+    }
+    else
+    {
+        printf("The given number is odd");
+    }
+
+
+
+
+
+    }

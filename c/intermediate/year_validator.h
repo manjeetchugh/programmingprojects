ea32d4297bd44060fceeaa055e476ef7a250ccdd
Manjeet Chugh <work.manjeetchugh@gmail.com>
2026-09-20 20:17:44 +0530

    Create year_validator.h
    Work in Progress

---
+++ c/intermediate/year_validator.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{   char pref;
+    int dd,mm,year;
+
+    printf("What format do you prefer? DD-MM-YYYY(A)MM-DD-YYYY(B) OR YYYY-MM-DD(I)\n Enter A,B,I"):
+    scanf("%c",&pref);
+    if
+    if((dd>0 && dd<32)&&(mm>0 && mm<13)&&(year>0 && year<9999))
+ {
+    if(mm==1 || mm==3||mm==5||mm==8||mm==10||mm==12)
+    {
+        if(dd==31)
+        {
+            printf("")
+        }
+    }
+ }
+
+
+} 

4811ce9925ed4ab962249097bd23191dc0e4dc7b
Manjeet Chugh <work.manjeetchugh@gmail.com>
2026-09-20 19:36:00 +0530

    Create leap_year.h

---
+++ c/easy/leap_year.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+    int yr;
+    printf("Enter Year\n");
+    scanf("%d",&yr);
+    if (yr%400==0)
+    {
+       printf("%d is a leap year.\n",yr);
+    }
+    else if(yr%100==0)
+    {
+        printf("%d is not a leap year.\n",yr);
+    }
+    else if(yr%4==0)
+    {
+        printf("%d is a leap year.\n",yr);
+    }
+    else 
+    {
+         printf("%d is not a leap year.\n",yr);
+    }
+    
+}

61728a131d490d947bf7c7880ce77da6a4816f99
Manjeet Chugh <work.manjeetchugh@gmail.com>
2026-09-21 19:49:22 +0530

    Create library_overdue_fine.h

---
+++ c/easy/library_overdue_fine.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    float fine = 0;
+
+    printf("Enter the number of days the book is overdue: ");
+
+    if (scanf("%d", &n) != 1 || n < 0)
+    {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    for (int day = 1; day <= n; day++)
+    {
+        if (day <= 5)
+        {
+            fine += 2;
+        }
+        else if (day <= 10)
+        {
+            fine += 5;
+        }
+        else
+        {
+            fine += 10;
+        }
+    }
+
+    printf("Fine: %.2f\n", fine);
+
+    return 0;
+}
